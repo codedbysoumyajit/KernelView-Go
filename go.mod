18826705fcc3589f471e132ebdb53607da736829
Soumyajit Das <sd490405@gmail.com>
2026-07-01 09:30:40 +0530

    hotfix

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.25.1
 
 require (
 	github.com/shirou/gopsutil/v3 v3.24.5
+	golang.org/x/sys v0.37.0
 	golang.org/x/term v0.36.0
 )
 
@@ -15,5 +16,4 @@ require (
 	github.com/tklauser/go-sysconf v0.3.12 // indirect
 	github.com/tklauser/numcpus v0.6.1 // indirect
 	github.com/yusufpapurcu/wmi v1.2.4 // indirect
-	golang.org/x/sys v0.37.0 // indirect
 )
--- init_windows.go
+++ init_windows.go
@@ -4,23 +4,24 @@ package main
 
 import (
 	"os"
-	"syscall"
+
+	"golang.org/x/sys/windows"
 )
 
 func init() {
 	// Enable Virtual Terminal Processing for Windows Console (ANSI Escape Sequences)
-	handle := syscall.Handle(os.Stdout.Fd())
+	handle := windows.Handle(os.Stdout.Fd())
 	var mode uint32
-	if err := syscall.GetConsoleMode(handle, &mode); err == nil {
+	if err := windows.GetConsoleMode(handle, &mode); err == nil {
 		mode |= 0x0004 // ENABLE_VIRTUAL_TERMINAL_PROCESSING
-		syscall.SetConsoleMode(handle, mode)
+		windows.SetConsoleMode(handle, mode)
 	}
 
 	// Also enable virtual terminal processing on Stderr in case warnings/errors are printed
-	handleStderr := syscall.Handle(os.Stderr.Fd())
+	handleStderr := windows.Handle(os.Stderr.Fd())
 	var modeStderr uint32
-	if err := syscall.GetConsoleMode(handleStderr, &modeStderr); err == nil {
+	if err := windows.GetConsoleMode(handleStderr, &modeStderr); err == nil {
 		modeStderr |= 0x0004
-		syscall.SetConsoleMode(handleStderr, modeStderr)
+		windows.SetConsoleMode(handleStderr, modeStderr)
 	}
 }
